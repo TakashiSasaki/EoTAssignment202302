0096e2319efd09de91c003638ab4e7f868002cf2
Takashi SASAKI <takashi316@gmail.com>
2023-02-12 12:44:58 +0900

    DatabaseActivityを実装。

--- app/src/main/java/com/example/toeassignment202302/DatabaseActivity.java
+++ app/src/main/java/com/example/toeassignment202302/DatabaseActivity.java
@@ -2,15 +2,90 @@ package com.example.toeassignment202302;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.allinone202302.R;
 
 public class DatabaseActivity extends AppCompatActivity {
 
+    //ダミーの値を設定しておく
+    private int _cocktailId = -1;
+    private String _cocktailName = "";
+    private DatabaseHelper _helper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
+
+        ((ListView) findViewById(R.id.lvCocktail))
+                .setOnItemClickListener(new ListItemClickListener());
+
+        _helper = new DatabaseHelper(this);
+
+    }
+
+    public void onSaveButtonClick(View v) {
+        SQLiteDatabase db = _helper.getWritableDatabase();
+
+        SQLiteStatement sqLiteStatement
+                = db.compileStatement("DELETE FROM cocktailmemos WHERE _id = ?");
+        sqLiteStatement.bindLong(1, _cocktailId);
+        sqLiteStatement.executeUpdateDelete();
+
+        String note = ((EditText) findViewById(R.id.etNote)).getText().toString();
+
+        SQLiteStatement sqLiteStatement1
+                = db.compileStatement(
+                "INSERT INTO cocktailmemos (_id, name, note) VALUES (?,?,?)");
+        sqLiteStatement1.bindLong(1, _cocktailId);
+        sqLiteStatement1.bindString(2, _cocktailName);
+        sqLiteStatement1.bindString(3, note);
+        sqLiteStatement1.executeInsert();
+
+        ((EditText) findViewById(R.id.etNote)).setText("");
+        ((TextView) findViewById(R.id.tvCocktailName)).setText(R.string.tv_name);
+        ((Button) findViewById(R.id.btnSave)).setEnabled(false);
+    }
+
+
+    class ListItemClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            _cocktailId = position;
+            _cocktailName = (String) parent.getItemAtPosition(position);
+
+            SQLiteDatabase readableDatabase = _helper.getReadableDatabase();
+            Cursor cursor = readableDatabase.rawQuery
+                    ("SELECT * FROM cocktailmemos WHERE _id = " + _cocktailId, null);
+
+            String note = "";
+
+            while (cursor.moveToNext()) {
+                int idxNote = cursor.getColumnIndex("note");
+                note = cursor.getString(idxNote);
+            }
+            cursor.close();
+
+            ((EditText) (findViewById(R.id.etNote))).setText(note);
+            ((TextView) (findViewById(R.id.tvCocktailName))).setText(_cocktailName);
+            findViewById(R.id.btnSave).setEnabled(true);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        _helper.close();
+        super.onDestroy();
     }
 }//DatabaseActivity
--- app/src/main/java/com/example/toeassignment202302/MainActivity.java
+++ app/src/main/java/com/example/toeassignment202302/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.toeassignment202302;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.allinone202302.R;
 
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+    }//onCreate
 
-}
+    public void startDatabaseActivity(View view) {
+        startActivity(new Intent(this, DatabaseActivity.class));
+    }//startDatabaseActivity
+}//MainActivity
